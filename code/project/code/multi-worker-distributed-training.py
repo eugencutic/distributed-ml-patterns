77e408056e83cb1b2aa22c3d98009aa47a7ddb02
eugencutic <35118018+eugencutic@users.noreply.github.com>
2026-09-05 18:51:53 +0200

    Fix SavedModel export with distributed variables

--- code/project/code/multi-worker-distributed-training.py
+++ code/project/code/multi-worker-distributed-training.py
@@ -192,17 +192,15 @@ def main(args):
     # Save to a path that is unique across workers.
     model_path = args.saved_model_dir + '/worker_tmp_' + str(TASK_INDEX)
 
-  multi_worker_model.save(model_path)
-
-
+  serve_image_fn = _get_serve_image_fn(multi_worker_model)
   signatures = {
-    "serving_default": _get_serve_image_fn(multi_worker_model).get_concrete_function(
-        tf.TensorSpec(shape=[None], dtype=tf.string, name='image_bytes')
-    )
+    "serving_default": serve_image_fn
   }
 
-  # https://www.tensorflow.org/api_docs/python/tf/saved_model/save
-  tf.saved_model.save(multi_worker_model, model_path, signatures=signatures)
+  multi_worker_model.save(
+    model_path,
+    signatures=signatures
+  )
 
 
 if __name__ == '__main__':
